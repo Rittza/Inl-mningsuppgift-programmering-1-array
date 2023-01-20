b10551eefd71b92008611f9163a521cf953d59e2
Meysam03 <alre03001@utb.vaxjo.se>
2023-01-20 10:24:20 +0100

    Add project files.

---
+++ Inlämningsuppgift programmering 1 array/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Inlämningsuppgift
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            Console.WriteLine("Hur många meningar vill du skriva in?");
+            int antal = int.Parse(Console.ReadLine());
+            string[] mening = new string[antal];
+            for(int i = 0; i < antal; i++)
+            {
+                Console.WriteLine("Skriv in en mening");
+                mening[i] = Console.ReadLine();
+            }
+            Console.WriteLine("Detta är dina meningar");
+            for(int i = 0; i < antal; i++)
+            {
+                Console.WriteLine(mening[i]);
+            }
+        }
+    }
+}
